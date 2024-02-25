53747dbd8677c7606152e890ea1a03e8f0884148
32489cc <1254902153gxc@gmail.com>
2024-02-25 17:35:51 +0900

    list

--- list.py
+++ list.py
@@ -1,2 +1,17 @@
 mylist=[[1,2],[3,4]]
-print(mylist.index([1,2]))
+# print(mylist.index([1,2]))
+# mylist[0]=[3,4,5,6,7]
+# print(mylist)
+list2=['extend',[123,456],222]
+mylist.extend(list2)
+print(mylist)
+newlist=mylist.pop(0)
+print(newlist)
+
+
+
+
+
+re_list=[1,1,1,1,2,3,4]
+
+print(re_list.count(1))
